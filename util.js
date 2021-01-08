028fb64b88a1f0cb41a30a1ff91b5fb610ce00a7
zzhoouxin <zxh87516@qq.com>
2021-01-08 15:13:46 +0800

    util

--- util.js
+++ util.js
@@ -2,16 +2,6 @@
 
 var _ = require('lodash');
 var prettier = require('prettier')
-
-/**
- * Recursively converts a swagger type description into a typescript type, i.e., a model for our mustache
- * template.
- *
- * Not all type are currently supported, but they should be straightforward to add.
- *
- * @param swaggerType a swagger type definition, i.e., the right hand side of a swagger type definition.
- * @returns a recursive structure representing the type, which can be used as a template model.
- */
 function convertType(swaggerType, swagger) {
   var typespec = {
     description: swaggerType.description,
@@ -59,12 +49,6 @@ function convertType(swaggerType, swagger) {
         typespec.properties.push(property);
       });
   }
-  /* Else {
-      // type unknown or unsupported... just map to 'any'...
-      typespec.tsType = 'any';
-      } */
-
-  // Since Mustache does not provide equality checks, we need to do the case distinction via explicit booleans
   typespec.isRef = typespec.tsType === 'ref';
   typespec.isObject = typespec.tsType === 'object';
   typespec.isArray = typespec.tsType === 'array';
