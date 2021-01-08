8e0681f8ae0eb089602d1b82b5ce02d09678eeca
zzhoouxin <zxh87516@qq.com>
2021-01-08 14:45:19 +0800

    add example

--- example/demo.ts
+++ example/demo.ts
@@ -1,6 +1,9 @@
 /**
  * 例子  这样可以完美支持ts
  */
-import api from '../controller/api'
-api.HomeController.homeGetName();
+import api from '../controller/api';
 
+async function test() {
+  const data = await api.HomeController.homeGetName();
+  console.log(data.age);
+}
